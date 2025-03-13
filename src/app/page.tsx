5cff11d3a6f492743f228bc2dbc05ebc5475d3a0
jcarew97 <40368129+jcarew97@users.noreply.github.com>
2025-03-12 20:49:28 -0400

    change colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,12 +47,12 @@ const Page: React.FC = () => {
   }, [messages, gotMessages]);
 
   return (
-    <div className="flex flex-col justify-between h-screen bg-gray-800 p-2 mx-auto max-w-full">
+    <div className="flex flex-col justify-between h-screen bg-cyan-900 p-2 mx-auto max-w-full">
       <Header className="my-5" />
 
       <button
         onClick={() => setModalOpen(true)}
-        className="fixed right-4 top-4 md:right-6 md:top-6 text-xl text-white animate-pulse-once info-button"
+        className="fixed right-4 top-4 md:right-6 md:top-6 text-xl text-grey-800 animate-pulse-once info-button"
       >
         <AiOutlineInfoCircle />
       </button>
